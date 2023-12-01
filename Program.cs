8f8a1a3c99d01e74d97551c70d0a4085d14965ee
Thiago <thiagomvas@gmail.com>
2023-12-01 18:39:57 -0300

    Format output with ConsoleTables. Add Summary Table

--- Program.cs
+++ Program.cs
@@ -1,13 +1,44 @@
 ﻿using System.CommandLine;
+using ConsoleTables;
 
 namespace codecountercli
 {
     internal class Program
     {
+
+        static readonly Dictionary<string, string> FileNames = new()
+        {
+            { "cs", "C#" },
+            { "java", "Java" },
+            { "cpp", "C++" },
+            { "py", "Python" },
+            { "js", "JavaScript" },
+            { "html", "HTML" },
+            { "css", "CSS" },
+            { "php", "PHP" },
+            { "swift", "Swift" },
+            { "rb", "Ruby" },
+            { "go", "Go" },
+            { "c", "C" },
+            { "h", "Header" },
+            { "ts", "TypeScript" },
+            { "jsx", "React JSX" },
+            { "tsx", "React TypeScript" },
+            { "vue", "Vue.js" },
+            { "json", "JSON" },
+            { "xml", "XML" },
+            { "sql", "SQL" },
+            { "pl", "Perl" },
+            { "sh", "Shell" },
+            { "yaml", "YAML" },
+            { "md", "Markdown" }
+        };
+
         private static async Task Main(string[] args)
         {
             var rootCommand = new RootCommand("A CLI that counts all the lines of code, excluding comments, in a folder");
 
+            // Options
             Option<string> filetypesOption = new(name: "--ignorefiles",
                 description: "A list of file extensions to ignored, separated by a comma (,)",
                 getDefaultValue: () => ".cs, .java, .cpp, .py, .js, .html, .css, .php, .swift, .rb, .go, .c, .h, .ts, .jsx, .tsx, .vue, .json, .xml, .sql, .pl, .sh, .yaml, .md");
@@ -16,18 +47,47 @@ namespace codecountercli
             rootCommand.Add(filetypesOption);
             rootCommand.Add(folderOption);
 
+
+            // Root Command Handler
             rootCommand.SetHandler((filetypes, folder) => {
 
-                string[] fileExtensions = filetypes.Replace(" ", "").Split(",");
+                string[] fileExtensions = filetypes.Replace(" ", "").Split(","); // Splits file types into an array of strings
 
                 Console.WriteLine($"Started search at {folder} ");
 
-                List<string> files = DirSearch(folder, fileExtensions);
+                List<string> files = DirSearch(folder, fileExtensions); // Searches for files in the folder
+                Dictionary<string, int> lineCountPerType = new();
 
                 Console.WriteLine($"Found {files.Count} files matching query:\n {filetypes}");
-                foreach (var file in files)
-                        Console.WriteLine($"File: {file} - {CountLines(file)} lines");
+                if (files.Count == 0) return;
 
+                ConsoleTable table = new("File Name", "Line Count", "File Type");
+
+                foreach (var file in files)
+                {
+                    string fileName = Path.GetFileName(file);
+                    string fileType = fileName.Split(".").Last();
+                    int lineCount = CountLines(file);
+
+                    if (lineCountPerType.ContainsKey(fileType))
+                        lineCountPerType[fileType] += lineCount;
+                    else
+                        lineCountPerType.Add(fileType, lineCount);
+
+                    table.AddRow(fileName, lineCount, FileNames.TryGetValue(fileType, out var name) ? name : fileType);
+                }
+
+                table.Write();
+
+                ConsoleTable summaryTable = new("File Type", "Line Count");
+
+                foreach (var (key, value) in lineCountPerType)
+                {
+                    summaryTable.AddRow(FileNames.TryGetValue(key, out var name) ? name : key, value);
+                }   
+
+                Console.WriteLine("\n Summary: ");
+                summaryTable.Write();
 
             }, filetypesOption, folderOption);
 
