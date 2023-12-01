99169ce8c7a6e3ef485f5abcd7b356cdcaa63da2
Thiago <thiagomvas@gmail.com>
2023-12-01 17:56:39 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System.CommandLine;
+
+namespace codecountercli
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var rootCommand = new RootCommand("A CLI that counts all the lines of code, excluding comments, in a folder");
+
+            Option<string> filetypesOption = new(name: "--ignorefiles",
+                description: "A list of file extensions to ignored, separated by a comma (,)",
+                getDefaultValue: () => ".cs, .java, .cpp, .py, .js, .html, .css, .php, .swift, .rb, .go, .c, .h, .ts, .jsx, .tsx, .vue, .json, .xml, .sql, .pl, .sh, .yaml, .md");
+            Option<string> folderOption = new(name: "--folder", description: "The target folder", getDefaultValue: Directory.GetCurrentDirectory);
+
+            rootCommand.Add(filetypesOption);
+            rootCommand.Add(folderOption);
+
+            rootCommand.SetHandler((filetypes, folder) => {
+
+                string[] fileExtensions = filetypes.Replace(" ", "").Split(",");
+
+                Console.WriteLine($"Started search at {folder}");
+
+                List<string> files = DirSearch(folder, fileExtensions);
+
+                Console.WriteLine($"Found {files.Count} files.");
+                foreach (var file in files)
+                        Console.WriteLine(file);
+
+
+            }, filetypesOption, folderOption);
+
+            await rootCommand.InvokeAsync(args);
+        }            
+
+        static List<string> DirSearch(string sDir, string[] fileTypeExtensions)
+        {
+            List<string> files = new List<string>();
+            try
+            {
+                foreach (string f in Directory.GetFiles(sDir))
+                {
+                    if(fileTypeExtensions.Any(x => f.EndsWith(x)))
+                        files.Add(f);
+                }
+                foreach (string d in Directory.GetDirectories(sDir))
+                {
+                    files.AddRange(DirSearch(d, fileTypeExtensions));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error when searching for files:\n{e}");
+            }
+
+            return files;
+        }
+    }
+}
